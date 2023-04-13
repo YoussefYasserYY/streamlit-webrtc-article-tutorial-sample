5a0c27ba5dbdf6d474f5e959c0aa1b1d3050b0c6
Youssef <84783747+YoussefYasserYY@users.noreply.github.com>
2023-04-13 09:56:53 +0200

    commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
-        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
+        img = cv2.cvtColor(cv2.COLOR_GRAY2BGR)
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
@@ -27,6 +27,3 @@ ctx = webrtc_streamer(
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
 )
-if ctx.video_processor:
-    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
